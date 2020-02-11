b32abf82e76cb03e8442ca939b5681d8509af7e9
CallumCathrine <60232241+CallumCathrine@users.noreply.github.com>
2020-02-11 18:34:07 +0000

    Something that works?

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -6,8 +6,19 @@ namespace assignment
     class Program
     {
         static void Main(string[] args){
-            string[] countries = { "Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus" };
-            Console.WriteLine(countries[0]);
+            List<Country> countries = new List<Country>();
+            countries.Add(new Country("Austria", 1.98));        
+                }
+    }
+
+    internal class Country
+    {
+        public Country(string name, double population)
+        {
+            Name = name;
+            Population = population;
         }
+        public string Name { get; set; }
+        public double Population { get; set; }
     }
 }
