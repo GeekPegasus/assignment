42ab167337bb3c24b6608aeb0d30c11718814c24
CallumCathrine <60232241+CallumCathrine@users.noreply.github.com>
2020-02-11 17:43:58 +0000

    Updates?
    NOT A FUCKING CLUE

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace assignment
 {
+    class Countries{ 
+        public
+    }
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("test");
+        static void Main(string[] args){
+            
+            
+            Dictionary<int, Countries> aDictionary = new Dictionary<int, Countries>();
+            aDictionary.Add(1, new Countries() {Key = "Austria", Population = 16});
+
+            foreach(KeyValuePair<int, Countries> c in aDictionary){
+                Console.WriteLine($"{c.Key} : {c.Population}");
+            }
         }
     }
 }
