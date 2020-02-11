1a7cb3c1d9eaf9dacc0f7c8701b5710b662c64cb
CallumCathrine <60232241+CallumCathrine@users.noreply.github.com>
2020-02-11 18:16:39 +0000

    IDFK ANYMORE

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -3,20 +3,10 @@ using System.Collections.Generic;
 
 namespace assignment
 {
-    class Countries{ 
-        public
-    }
     class Program
     {
         static void Main(string[] args){
-            
-            
-            Dictionary<int, Countries> aDictionary = new Dictionary<int, Countries>();
-            aDictionary.Add(1, new Countries() {Key = "Austria", Population = 16});
-
-            foreach(KeyValuePair<int, Countries> c in aDictionary){
-                Console.WriteLine($"{c.Key} : {c.Population}");
-            }
+            Console.WriteLine("Hello!");
         }
     }
 }
